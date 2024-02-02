0c4693fb7342657f1139778d12903b62eeb17e7c
Iñigo Taibo <hello@itaibo.com>
2024-02-02 14:33:00 +0100

    Check if production

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const baseUrl = 
-  process.env.NEXT_PUBLIC_VERCEL_URL
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
     ?
-      'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
+      'https://decashop.vercel.app'
     :
       'http://localhost:3000';
